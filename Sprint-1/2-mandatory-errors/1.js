5df9b40a8609b399fb794176db87b35241c7e2c7
Zadri Abdule <zadriabdule@gmail.com>
2026-02-17 13:25:27 +0000

    Update 1.js

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -3,8 +3,9 @@
 
 
 let age = 33;
-age = age + 1;
-console.log(age);
+
+age++; // Increases age by 1
+
 
 
 // Changed const to let because the variables value need to change during the program. const creates a fixed reference that cannot be reassigned. but let allows updating the variable which is required when incrementing or modifying values.
