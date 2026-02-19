a4dbb5a008328445624cae0f0e35af333bc48a87
Zadri Abdule <zadriabdule@gmail.com>
2026-02-19 14:07:15 +0000

    Update 3-paths.js

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -18,7 +18,10 @@ console.log(`The base part of ${filePath} is ${base}`);
 // Create a variable to store the ext part of the variable
 
 const dir = filePath.slice(0, lastSlashIndex);
-const ext = filePath.slice(lastDotIndex);
+console.log(`The dir part of ${filePath} is ${dir}`);
+
+const ext = filePath.slice(lastDotIndex + 1);
+console.log(`The ext part of ${filePath} is ${ext}`);
 
 
 
