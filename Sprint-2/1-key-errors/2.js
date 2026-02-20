34bc832dd6bdb4edf99e5990d5c7df0c48290534
Zadri Abdule <zadriabdule@gmail.com>
2026-02-20 12:56:29 +0000

    Update 2.js

--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -14,7 +14,7 @@ function square(3) {
 // =============> explain this error message here
 // The "Unexpected number"
 // Finally, correct the code to fix the problem
-// The "Unexpected number" error arises when a number is improperly positioned or used within JvaScript code
+// The "Unexpected number" error arises when a number is improperly positioned or used within JavaScript code
 // =============> write your new code here
  function square(num){
       return num * num;
