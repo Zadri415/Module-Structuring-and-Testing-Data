97a5210973b9ee2f6e0ea5a6936ae284fb7e7521
Zadri <zadriabdule@gmail.com>
2026-03-15 15:43:19 +0000

    implement getOrdinalNumber function

--- Sprint-3/2-practice-tdd/get-ordinal-number.js
+++ Sprint-3/2-practice-tdd/get-ordinal-number.js
@@ -1,5 +1,18 @@
 function getOrdinalNumber(num) {
-  return "1st";
+  const lastDigits = num % 100;
+  if (lastDigits >= 11 && lastDigits <= 13) {
+    return num + "th";
+  }
+  switch (lastDigits % 10) {
+    case 1:
+      return num + "st";
+    case 2:
+      return num + "nd";
+    case 3:
+      return num + "rd";
+    default:
+      return num + "th";
+  }
 }
 
 module.exports = getOrdinalNumber;
